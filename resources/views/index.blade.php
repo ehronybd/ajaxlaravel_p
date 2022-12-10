ea29cf6fb9c5a8555b44e1cd503538c95bcda9c1
ehronybd <ehronybd@gmail.com>
2022-12-10 23:04:12 +0600

    https://dcodemania.com/post/crud-application-image-upload-laravel-8-ajax-sweetalert-datatable
    https://www.youtube.com/watch?v=z8cCuWz_fYA

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD App Laravel 8 & Ajax</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css' />
+  <link rel='stylesheet'
+    href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css' />
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.css" />
+
+</head>
+@include('add_user')
+@include('edit_user')
+
+<body class="bg-light">
+  <div class="container">
+    <div class="row my-5">
+      <div class="col-lg-12">
+        <div class="card shadow">
+          <div class="card-header bg-danger d-flex justify-content-between align-items-center">
+            <h3 class="text-light">Manage Employees</h3>
+            <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#addEmployeeModal"><i
+                class="bi-plus-circle me-2"></i>Add New Employee</button>
+          </div>
+          <div class="card-body" id="show_all_employees">
+            <h1 class="text-center text-secondary my-5">Loading...</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ @include('script_js')
+</body>
+
+</html>
